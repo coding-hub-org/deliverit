adfafd704e1a00f40e6e79a2a32f60ac624e53a1
Gaurav Jayasawal <jgaurav6@gmail.com>
2019-02-10 14:44:39 -0500

    made login component files

--- deliverit-frontend/src/App.js
+++ deliverit-frontend/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Login from "./components/Login/Login";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         Welcome to DeliverIt.
+
+        These are the new changes in LIfe.
+
+        <Login />
       </div>
     );
   }
