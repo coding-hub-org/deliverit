0a89380a7eb9c705e340d5d1dd8053575632f028
Keith Prevost <rickyprev@yahoo.com>
2019-02-19 14:41:52 -0500

    started Structure for login page

--- deliverit-frontend/src/App.js
+++ deliverit-frontend/src/App.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Login from "./components/Login/Login";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Welcome to DeliverIt.
-
-        These are the new changes in LIfe.
-
         <Login />
       </div>
     );
