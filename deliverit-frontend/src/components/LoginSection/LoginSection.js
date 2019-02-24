98fc11b02b22133ce97e44eb8db22eb0a323c3af
Keith Prevost <rickyprev@yahoo.com>
2019-02-24 12:34:28 -0500

    loginSection without mediaQuery

--- deliverit-frontend/src/components/LoginSection/LoginSection.js
+++ deliverit-frontend/src/components/LoginSection/LoginSection.js
@@ -4,33 +4,48 @@ import "./LoginSection.css";
 class Login extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="loginWrapper">
-          <form className="inputForm">
-            <h1 id="formHeader">Login</h1>
-            <h2>Username</h2>
-            <input
-              className="userLogin"
-              type="text"
-              name="userName"
-              placeholder="Username here"
-            />
-            <h2>Password</h2>
-            <input
-              className="userLogin"
-              type="text"
-              name="password"
-              placeholder="**********"
-            />
-            <button className="loginButton">Login</button>
-            <h3>
-              Forgot your password?<span className="textLink"> Click here</span>
-            </h3>
+      <div className="LoginSection-section">
+        <div className="LoginSection-section--loginWrapper">
+          <div className="LoginSection-section--loginWrapper__birdLogo">
+           
+            <img src={require("../../assets/logo.png")} />
+          </div>
+          <div className="LoginSection-section--loginWrapper__alignDiv ">
+            <form className="loginForm">
+              <h1 id="formHeader">Login</h1>
+              <h2>Username</h2>
+              <input
+                className="userLogin"
+                type="text"
+                name="userName"
+                placeholder="Username here"
+              />
+              <h2>Password</h2>
+              <input
+                className="userLogin"
+                type="text"
+                name="password"
+                placeholder="**********"
+              />
+              <button className="loginButton">Login</button>
+              <h3>
+                Forgot your password?
+                <span className="textLink"> Click here</span>
+              </h3>
 
-            <h4>
-              Dont have an account yet?<span> Register here</span>
-            </h4>
-          </form>
+              <h4>
+                Dont have an account yet?<span> Register here</span>
+              </h4>
+            </form>
+
+          </div>
+          <div className="leaningLogo">
+           
+          <img
+              src={require("../../assets/login-img.svg")}
+              className="leaningMan"
+            />
+        </div>
         </div>
       </div>
     );
