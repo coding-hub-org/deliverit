25802a301354f0613f1d3040f3dc54957c946222
Gaurav Jayasawal <jgaurav6@gmail.com>
2019-02-24 13:21:49 -0500

    3rd try git ignore

---
+++ deliverit-frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './sections/Header/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        Welcome to DeliverIt.
+
+        These are the new changes in LIfe.
+        WORK WORK WORK
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
