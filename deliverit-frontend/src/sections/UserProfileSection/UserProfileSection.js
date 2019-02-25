019c2d15d608dc9304741300e744546924695a1b
Hung Nguyen <nthungdev@gmail.com>
2019-02-25 16:05:08 -0500

    update

--- deliverit-frontend/src/sections/UserProfileSection/UserProfileSection.js
+++ deliverit-frontend/src/sections/UserProfileSection/UserProfileSection.js
@@ -8,13 +8,20 @@ class UserProfileSection extends Component {
         <div className="user-profile-navbar">NavBar</div>
         <div className="user-profile-footer">Footer</div>
         <div className="user-profile-section">
-          <div className="user-profile-section-title" />
-          <div className="user-profile-section-photo" />
+          <div className="user-profile-section-title">
+            <h1>Account</h1>
+          </div>
+          <div className="user-profile-section-photo">
+            <div className="user-profile-section-photo-frame" />
+            <button className="user-profile-section-photo-button">
+              Change Photo
+            </button>
+          </div>
           <div className="user-profile-section-content">
             <div className="user-profile-section-rating" />
             <div className="user-profile-section-stats" />
             <div className="user-profile-section-account-information">
-              <div className="user-profile-section-title" />
+              <div className="user-profile-section-name" />
               <div className="user-profile-section-details" />
               <div className="user-profile-section-address" />
               <div className="user-profile-section-payment" />
