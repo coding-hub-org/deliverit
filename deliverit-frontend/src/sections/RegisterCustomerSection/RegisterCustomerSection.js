18ddcc24a7238a8d7d2731ebe62fe6b3465d45d3
Keenan Kunzelman <keekunz@gmail.com>
2019-02-25 19:37:41 -0500

    finished page just need to fix naming conventions. I also accidently changed Footer.css just because my code formatter did it when I looked at the file and saved it. The change does not actually effect how it works

--- deliverit-frontend/src/sections/RegisterCustomerSection/RegisterCustomerSection.js
+++ deliverit-frontend/src/sections/RegisterCustomerSection/RegisterCustomerSection.js
@@ -4,29 +4,51 @@ import FormImage from "../../assets/register-img.svg";
 import "./RegisterCustomerSection.css";
 import LoginRegisterHeader from "../../components/LoginRegisterHeader/LoginRegisterHeader";
 
-
 const RegisterCustomerSection = props => {
   return (
     <div className="RegisterCustomerSection">
       <LoginRegisterHeader />
       <div className="FormWrapperGrid">
-       
-        <div className="FormGrid">
-        <h1 className="FormLabel">Register</h1>
         <form action="">
-          <input type="text" id="FullName"/>
-          <input type="tel" name="" id="Phone"/>
-          <input type="email" name="" id="Email"/>
-          <input type="text" id="NextPhone"/>
-          <input type="password" name="" id="Password"/>
-          <input type="password" name="" id="ConfirmPassword"/>
+          <div className="FormGrid">
+            <h1 className="FormLabel">Register</h1>
+            <div id="input-div">
+              <label for="name">Full name</label>
+              <br />
+              <input type="text" name="name" id="FullName" />
+            </div>
+            <div id="input-div">
+              <label for="phone">Phone</label>
+              <br />
+              <input type="tel" name="phone" id="Phone" />
+            </div>
+            <div id="input-div">
+              <label for="email">Email</label> <br />
+              <input type="email" name="email" id="Email" />
+            </div>
+            <div id="input-div">
+              <label for="phone">Phone</label> <br />
+              <input type="text" name="phone" id="NextPhone" />
+            </div>
+            <div id="input-div">
+              <label for="password">Password</label> <br />
+              <input type="password" name="password" id="Password" />
+            </div>
+            <div id="input-div">
+              <label for="cpassword">Confirm password</label> <br />
+              <input type="password" name="cpassword" id="ConfirmPassword" />
+            </div>
+            <button type="submit">REGISTER</button>
+
+            <p>
+              Have an account already? <a href="">Login here</a>
+            </p>
+          </div>
         </form>
-       
-        </div>
-        <img className="FormLogo" src={FormImage} alt=""/>
+
+        <img className="FormLogo" src={FormImage} alt="" />
       </div>
     </div>
-    
   );
 };
 
