83685125cb7ce697e62e651f9fe7fc36ef2530ab
Gaurav Jayasawal <jgaurav6@gmail.com>
2019-04-19 23:52:20 -0400

    Added some comments

--- deliverit-backend/routes/images.js
+++ deliverit-backend/routes/images.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require("../database/database");
 const queries = require("../database/queries");
 
+// This method returns all the list of stores as json
 router.get("/", function(req, res, next) {
   let query;
   async function queryCall() {
