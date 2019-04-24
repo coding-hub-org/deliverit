2d2cd9494dbd5f2032b2dc40c4d3faa3749110c8
winkhanh <giakhanhnk2000@gmail.com>
2019-04-24 12:39:57 -0400

    Redirect Sign Out

--- deliverit-frontend/src/components/SignOut/SignOut.js
+++ deliverit-frontend/src/components/SignOut/SignOut.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import { withFirebase } from '../Firebase'
 const buttonStyle={
   cursor: 'pointer'
 }
+
 const SignOutButton = ({ firebase }) => (
+  <Link to="/">
   <button type="button" onClick={firebase.doSignOut} style={buttonStyle}>
     Sign Out
   </button>
+  </Link>
 );
 
 export default withFirebase(SignOutButton);
